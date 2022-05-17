93a85b508b74221daee5da18d8eb2fec79ba1af0
Kapil <kapilzade786@gmail.com>
2022-05-17 17:30:35 +0530

    --- Auto-Git Commit ---

---
+++ terraform/demo.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "webserver" {
+   ami = "ami-02541b8af977f6cdd"  
+   instance_type = "t2.micro"
+   security_groups = [ "default" ]
+   key_name = "californiakey.pem"
+}
