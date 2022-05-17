06c7c72f0dd025b238d338c6cb5900539a1cb1ff
Kapil <kapilzade786@gmail.com>
2022-05-17 18:11:02 +0530

    --- Auto-Git Commit ---

--- ansible/Coudblitz/demo.tf
+++ ansible/Coudblitz/demo.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "webserver" {
- ami = "ami-02541b8af977f6cdd"  
+  ami = "ami-02541b8af977f6cdd"  
   instance_type = "t2.micro"
   security_groups = [ "default" ]
   key_name = "californiakey.pem"
