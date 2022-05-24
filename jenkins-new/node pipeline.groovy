d2d9254b4d99a7e25d79f6fcc9469c128d85647e
Kapil <kapilzade786@gmail.com>
2022-05-24 09:25:47 +0530

    --- Auto Git Commit ---

---
+++ jenkins-new/node pipeline.groovy
@@ -0,0 +1,11 @@
+node {  
+    stage('Pull') { 
+      git branch: 'main', credentialsId: 'github', url: 'https://github.com/Kapilzade/student-app.git'   
+    }
+    stage('Build') { 
+        echo "build done"
+    }
+    stage('Deploy') { 
+        echo "Deploy Done"
+    }
+}
