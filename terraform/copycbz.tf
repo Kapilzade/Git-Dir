eae890af67d236e2309fe8c16c9e5e5943e654f2
Kapil <kapilzade786@gmail.com>
2022-05-26 08:27:00 +0530

    --- Auto Git Commit ---

--- terraform/copycbz.tf
+++ terraform/copycbz.tf
@@ -1,13 +1,15 @@
-provider "aws" {
-    region = "us-west-1"
+#
+#provider "aws" {
+    #region = "us-west-1"
     access_key = "key"
     secret_key = "key"
  
 }
-resource "aws_instance" "webserver" {
-  ami = "ami-02541b8af977f6cdd"
+#resource "aws_instance" "webserver" {
+  #ami = "ami-02541b8af977f6cdd"
   instance_type = "t2.micro"
   tags = {
     "Name" = "instance1"
   }
 }
+##
