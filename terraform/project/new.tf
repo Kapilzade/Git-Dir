f74a8b530ba8da7f9c4f8018be5cc780d3310828
Kapil <kapilzade786@gmail.com>
2022-05-26 18:09:51 +0530

    --- Auto Git Commit ---

--- terraform/project/new.tf
+++ terraform/project/new.tf
@@ -5,7 +5,7 @@ force_destroy = true
 }
 
 resource "aws_iam_user_login_profile" "awsuser_profile" {
- user  = aws_iam_user.terraform
+ user  = aws_iam_user.terraform.name
  pgp_key  = "keybase:kapilzade"
   
 }
