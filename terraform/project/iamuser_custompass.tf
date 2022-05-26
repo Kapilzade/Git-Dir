567857612b44da0ab721d77ca5a1798ac04b4c5e
Kapil <kapilzade786@gmail.com>
2022-05-26 08:52:07 +0530

    --- Auto Git Commit ---

---
+++ terraform/project/iamuser_custompass.tf
@@ -0,0 +1,15 @@
+resource "aws_iam_user" "terraform" {
+name = "awsuser" 
+path = "/"
+force_destroy = true
+}
+
+resource "aws_iam_user_login_profile" "awsuser_profile" {
+    user = aws_iam_user.terraform
+    pgp_key = "keybase:kapilzade"
+  
+}
+ 
+output "password" {
+  value = aws_iam_user_login_profile.awsuser_profile.encrypted_password
+}
