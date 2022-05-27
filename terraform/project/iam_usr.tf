4e4fdc94f504c6ac6bc3d0ad66dded486343c9b1
Kapil <kapilzade786@gmail.com>
2022-05-27 09:48:47 +0530

    --- Auto Git Commit ---

--- terraform/project/iam_usr.tf
+++ terraform/project/iam_usr.tf
@@ -1,17 +1,17 @@
-resource "aws_iam_user" "terraform" {
+#resource "aws_iam_user" "terraform" {
 name = "awsuser" 
 path = "/"
 force_destroy = true
 }
 
-resource "aws_iam_user_login_profile" "awsuser_profile" {
+#resource "aws_iam_user_login_profile" "awsuser_profile" {
  user  = aws_iam_user.terraform.name
  pgpgp_key = "keybase:saurabh"
  
   
 }
  
-output "password" {
+#output "password" {
   value = aws_iam_user_login_profile.awsuser_profile.encrypted_password
 
 }
