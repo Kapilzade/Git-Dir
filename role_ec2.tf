e86598bf8f1e6ea4c60415c44250db3479840470
Kapil <kapilzade786@gmail.com>
2022-05-27 10:33:32 +0530

    --- Auto Git Commit ---

--- role_ec2.tf
+++ role_ec2.tf
@@ -1,3 +1,48 @@
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+  policy = jsonencode({
+      "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "ec2:*",
+            "Effect": "Allow",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "elasticloadbalancing:*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "cloudwatch:*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "autoscaling:*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "iam:CreateServiceLinkedRole",
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "iam:AWSServiceName": [
+                        "autoscaling.amazonaws.com",
+                        "ec2scheduled.amazonaws.com",
+                        "elasticloadbalancing.amazonaws.com",
+                        "spot.amazonaws.com",
+                        "spotfleet.amazonaws.com",
+                        "transitgateway.amazonaws.com"
+                    ]
+                }
+            }
+        }
+    ]
+  })
 resource "aws_iam_role" "ec2_role" {
   name =  "ec2_role"
   assume_role_policy = jsonencode({
@@ -13,4 +58,10 @@ resource "aws_iam_role" "ec2_role" {
           },
       ]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.policy_arn
+}
 }
