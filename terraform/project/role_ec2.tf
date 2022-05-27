cdb4772eda2d0932b1cdb3009fc0543bcd366c90
Kapil <kapilzade786@gmail.com>
2022-05-27 09:53:04 +0530

    --- Auto Git Commit ---

--- terraform/project/role_ec2.tf
+++ terraform/project/role_ec2.tf
@@ -2,7 +2,7 @@ resource "aws_iam_policy" "ec2_policy" {
     name = "ec2_policy1"
     path = "/"
     description = "policy attach ec2 and provide permission"
-    policy = jsoncode ({
+    policy = jsonencode ({
         version = "2012-10-17"
         statement = [
             {
