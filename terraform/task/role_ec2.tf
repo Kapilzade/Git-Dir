b21e4352eb2dd5a2d3f7aaf8bc3bb019ab2c8146
Kapil <kapilzade786@gmail.com>
2022-05-27 17:40:28 +0530

    --- Auto Git Commit ---

--- terraform/task/role_ec2.tf
+++ terraform/task/role_ec2.tf
@@ -49,6 +49,7 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   
 }
 resource "aws_instance" "app-server" {
+  name = "app_server"
   ami =  "ami-0fa49cc9dc8d62c84"
   instance_type = "t2.micro"
   key_name = "flame"
