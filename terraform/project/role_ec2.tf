7f96739758120bd51d8a17fe466cbb68e84998fe
Kapil <kapilzade786@gmail.com>
2022-05-27 09:30:43 +0530

    --- Auto Git Commit ---

---
+++ terraform/project/role_ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_iam_policy" "ec2_policy" {
+    name = "ec2_policy1"
+    path = "/"
+    description = "policy attach ec2 and provide permission"
+    policy = jsoncode
+    terraform import aws_iam_policy.administartor arn:aws:iam::aws:policy/AmazonEC2FullAccess 
+
+}
