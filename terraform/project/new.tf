e66530b37b980d25fb9b9e2dec2ed4a2dea87f01
Kapil <kapilzade786@gmail.com>
2022-05-27 16:27:01 +0530

    --- Auto Git Commit ---

--- terraform/project/new.tf
+++ terraform/project/new.tf
@@ -45,7 +45,7 @@ resource "aws_iam_role_policy_attachment" "test-attach" {
 
 resource "aws_iam_instance_profile" "ec2_profile" {
     name = "ec2_profile"
-    role = aws_iam_role.test-role.name
+    role = aws_iam_role.test-role
   
 }
 resource "aws_instance" "app-server" {
