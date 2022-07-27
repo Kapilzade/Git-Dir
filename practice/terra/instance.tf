5149a4ab04d8b30b413eeb37e11d93b68598f245
Kapil <kapilzade786@gmail.com>
2022-07-28 00:46:32 +0530

    --- Auto Git Commit ---

--- practice/terra/instance.tf
+++ practice/terra/instance.tf
@@ -3,7 +3,7 @@ region = "us-east-2"
 }
   resource "aws_instance" "app-server" {
   name = "app_server"
-  ami =  "ami-02d1e544b84bf7502"
+  ami =  "ami-02d1e544b84bf7502s"
   instance_type = "t2.micro"
   key_name = "flame"
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
