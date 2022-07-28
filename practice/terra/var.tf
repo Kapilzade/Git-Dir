84c34b6aa31a68fe77243f93397f629d8db32f24
Kapil <kapilzade786@gmail.com>
2022-07-28 16:02:47 +0530

    --- Auto Git Commit ---

--- practice/terra/var.tf
+++ practice/terra/var.tf
@@ -8,3 +8,12 @@ variable "instance_type_list" {
   type = list
   default = ["t2.micro","t3.small","t3.micro"]
 }
+
+variable "ami-id-map" {
+    type = "map"
+    default = {  
+      ubuntu = "ami-02f3416038bdb17fb"
+      rhel = "ami-092b43193629811af"
+      awsmachine = "ami-02d1e544b84bf7502"
+    }
+}
