1a27e63a1df573a797fe911378fe980f49a9ca15
Kapil <kapilzade786@gmail.com>
2022-07-28 12:26:42 +0530

    --- Auto Git Commit ---

--- practice/terra/main.tf
+++ practice/terra/main.tf
@@ -7,13 +7,3 @@ resource "aws_instance" "web" {
 }
 
 
-
-
-
-resource "aws_instance" "web" {
-    ami = "${var.ami-id}"
-    instance_type = "${var.instance-type-list[2]}"
-    tags = {
-        Name = "instance1"
-  }
-}
