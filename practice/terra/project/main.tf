76bedc034d84f237e7de52751a301f54e45f642e
Kapil <kapilzade786@gmail.com>
2022-07-30 01:35:23 +0530

    --- Auto Git Commit ---

--- practice/terra/project/main.tf
+++ practice/terra/project/main.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "private_subnet" {
 
 resource "aws_subnet" "public_subnet" {
   vpc_id = "${aws_vpc.my_vpc.id}"
-  cidr_block = "192.168.0.0/24"
+  cidr_block = "192.168.16.0/20"
   tags = {
     "Name" = "public_subnet"
     Env = "${var.env}"
